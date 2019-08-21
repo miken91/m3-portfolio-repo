a161495b4bb583050257675e851a87edc44a0463
Mike Noel <mike.e.noel3@gmail.com>
2019-08-20 20:08:11 -0500

    Importing header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "../components/footer";
 import { makeStyles } from "@material-ui/core/styles";
+import Header from "../components/header";
 
 const useStyles = makeStyles(theme => ({
   container: {
