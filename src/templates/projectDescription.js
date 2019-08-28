2d944128bf71f1e07c04f652d76eb7a3ff967741
Mike Noel <mike.e.noel3@gmail.com>
2019-08-27 22:02:53 -0500

    Fixing errors

--- src/templates/projectDescription.js
+++ src/templates/projectDescription.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
-import Img from "Gatsby-image";
+import Img from "gatsby-image";
 import Layout from "../components/layout";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { makeStyles } from "@material-ui/styles";
